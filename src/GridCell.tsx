4be448b3b4be816ff5c61f616a77a7ae82d3f1ba
Jakub Kukuryk <jakub@kukuryk.com>
2022-05-26 23:38:16 +0200

    improve scale

--- src/GridCell.tsx
+++ src/GridCell.tsx
@@ -43,7 +43,6 @@ export const GridCell: FunctionComponent<Props> = ({
     sourceHeight,
 }) => {
     const maskRef = useRef(null);
-    const [anchorTranslation] = useState({ x: 0, y: 0 });
 
     const draw = useCallback(
         (g) => {
@@ -61,15 +60,23 @@ export const GridCell: FunctionComponent<Props> = ({
     );
     const [scale, setScale] = useState(2);
     const [, setFrame] = useState(0);
-    const [currentScale, setCurrentScale] = useState(SUBDIVISION);
-    const startTime = useMemo(() => {
-        return Date.now() - turbulenceTime;
-    }, [turbulenceTime]);
+    const [currentScale, setCurrentScale] = useState(2);
+    const baseSize = useMemo(() => {
+        const ratioX = (SUBDIVISION * width) / sourceWidth;
+        const ratioY = (SUBDIVISION * height) / sourceHeight;
+        const r = Math.max(ratioX, ratioY);
+        return [sourceWidth * r, sourceHeight * r];
+    }, [height, sourceHeight, sourceWidth, width]);
 
+    const anchorBase = useMemo(() => {
+        const range = 1 / SUBDIVISION;
+        const anchorX = x * range + range / 2;
+        const anchorY = y * range + range / 2;
+        return [anchorX, anchorY];
+    }, [x, y]);
+    const [anchorScale, setAnchorScale] = useState(1);
     const mouseZoom = useCallback(() => {
-        const maxSpeed = Math.abs(currentScale - scale) / Math.min(currentScale, scale);
-
-        const speed = lerp(0.0001, maxSpeed, ZOOM_SPEED / 100);
+        const speed = lerp(0.000001, 0.1, ZOOM_SPEED / 100);
         if (currentScale > scale + speed) {
             setCurrentScale(currentScale - speed);
         } else if (currentScale < scale - speed) {
@@ -83,6 +90,7 @@ export const GridCell: FunctionComponent<Props> = ({
         mouseZoom();
         setFrame((c) => c + 1); //keep updating
     });
+
     useEffect(() => {
         const trX = mouseTranslate[0];
         const positionX = x * width + width / 2;
@@ -94,71 +102,62 @@ export const GridCell: FunctionComponent<Props> = ({
         const cellsRangeY = Math.max(ZOOM_RANGE_CELLS - rangeY / height, 0) / ZOOM_RANGE_CELLS;
         const cellsRangeX = Math.max(ZOOM_RANGE_CELLS - rangeX / width, 0) / ZOOM_RANGE_CELLS;
         const cellsRange = Math.max(cellsRangeY * cellsRangeX);
-        const minScale = 1 + getTurbulence() * 2;
         const imageZoom = getImageZoom();
+
+        setAnchorScale(lerp(CELL_IMAGE_ZOOM, SCALE_MOUSE_ZOOM, cellsRange));
         const mouseZoom = getMouseImageZoom(imageZoom);
         switch (DISPLAY) {
             case DisplayMode.GRID:
                 const scalePrcG = lerp(imageZoom, mouseZoom, cellsRange);
-                setScale(max([scalePrcG, minScale]));
+                setScale(scalePrcG);
                 break;
             case DisplayMode.COLUMN:
                 const scalePrcC = lerp(imageZoom, mouseZoom, cellsRangeX);
-                setScale(max([scalePrcC, minScale]));
+                setScale(scalePrcC);
                 break;
             case DisplayMode.ROW:
                 const scalePrcR = lerp(imageZoom, mouseZoom, cellsRangeY);
-                setScale(max([scalePrcR, minScale]));
+                setScale(scalePrcR);
                 break;
         }
     }, [height, mouseTranslate, position, sourceHeight, sourceWidth, width, x, y]);
 
+    const startTime = useMemo(() => {
+        return Date.now() - turbulenceTime;
+    }, [turbulenceTime]);
+    const anchor = useMemo(() => {
+        if (anchorScale < 0) {
+            const anchorTranslateX = lerp(anchorBase[0], 0.5, anchorScale / -100);
+            const anchorTranslateY = lerp(anchorBase[1], 0.5, anchorScale / -100);
+            return [anchorTranslateX, anchorTranslateY];
+        } else {
+            return anchorBase;
+        }
+    }, [anchorBase, anchorScale]);
+    const [turbulenceForce, setTurbulenceForce] = useState({ x: anchor[0], y: anchor[1] });
+
     const nextTranslation = useCallback(() => {
-        if (STIR_FREQUENCY > 0) {
-            const turbulenceStepTime = (STIR_FREQUENCY / 100) * (STIR_FREQUENCY_BASE_TIME * 1000);
+        if (STIR_FREQUENCY >= 0) {
+            const turbulenceStepTime = ((100 - STIR_FREQUENCY) / 100) * (STIR_FREQUENCY_BASE_TIME * 1000);
             const currentTime = Date.now() - startTime;
             const targetStep = Math.ceil(currentTime / turbulenceStepTime) % turbulence.length;
             const diffTime = currentTime % turbulenceStepTime;
-            const turbulenceRange = getTurbulence();
-            gsap.to(anchorTranslation, {
+            gsap.to(turbulenceForce, {
                 duration: (turbulenceStepTime - diffTime) / 1000,
-                ease: 'back.inOut',
-                y: (turbulence[targetStep][0] * turbulenceRange) / 2,
-                x: (turbulence[targetStep][1] * turbulenceRange) / 2,
+                ease: 'power1.inOut',
+                x: lerp(anchor[0], 1 - anchor[0], turbulence[targetStep][0] * (STIR_STRENGTH / 100)),
+                y: lerp(anchor[1], 1 - anchor[1], turbulence[targetStep][1] * (STIR_STRENGTH / 100)),
             }).then(nextTranslation);
         } else {
             setTimeout(nextTranslation, 1000);
+            setTurbulenceForce({ x: anchor[0], y: anchor[1] });
         }
-    }, [anchorTranslation, startTime]);
-
+    }, [anchor, startTime, turbulenceForce]);
     useEffect(() => {
         nextTranslation();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    const anchorValue = useMemo(() => {
-        const turbulenceRange = 1 - getTurbulence() * 2;
-        const turbulenceStart = getTurbulence();
-        let anchorX = turbulenceStart + (x / SUBDIVISION) * turbulenceRange;
-        let anchorY = turbulenceStart + (y / SUBDIVISION) * turbulenceRange;
-        if (DISPLAY === DisplayMode.ROW) {
-            anchorX = 0.5;
-        }
-        if (DISPLAY === DisplayMode.COLUMN) {
-            anchorY = 0.5;
-        }
 
-        return [anchorX, anchorY];
-    }, [x, y]);
-
-    const finalAnchor = useMemo(() => {
-        return [
-            anchorValue[0] + (DISPLAY === DisplayMode.ROW ? 0 : anchorTranslation.x),
-            anchorValue[1] + (DISPLAY === DisplayMode.COLUMN ? 0 : anchorTranslation.y),
-        ];
-    }, [anchorTranslation.x, anchorTranslation.y, anchorValue]);
-    const finalPosition = useMemo(() => {
-        return [-width / 2 + width * finalAnchor[0], -height / 2 + height * finalAnchor[1]];
-    }, [finalAnchor, height, width]);
     const flipX = useMemo(() => {
         if (x % 2 !== 0 && FLIP_CELLS) {
             return -1;
@@ -169,55 +168,36 @@ export const GridCell: FunctionComponent<Props> = ({
             return -1;
         } else return 1;
     }, [y]);
+
     return (
         <Container mask={maskRef?.current} position={[x * width, y * height]} scale={[flipX, flipY]}>
             <Graphics name="mask" draw={draw} ref={maskRef} />
             <Sprite
                 image={source}
-                position={[finalPosition[0], finalPosition[1]]}
-                anchor={[finalAnchor[0], finalAnchor[1]]}
-                width={width * (DISPLAY === DisplayMode.ROW ? 1 : currentScale)}
-                height={height * (DISPLAY === DisplayMode.COLUMN ? 1 : currentScale)}
+                anchor={[turbulenceForce.x, turbulenceForce.y]}
+                width={baseSize[0] * (DISPLAY === DisplayMode.ROW ? 1 : currentScale)}
+                height={baseSize[1] * (DISPLAY === DisplayMode.COLUMN ? 1 : currentScale)}
             />
         </Container>
     );
 };
-const maxTurbulence = 0.5;
-export function getTurbulence() {
-    return lerp(0, maxTurbulence, STIR_STRENGTH / 100);
-}
-function max(values: number[]): number {
-    let maxValue = 0;
-    values.forEach((val, key) => {
-        if (key === 0 && (val || val === 0)) {
-            maxValue = val;
-        } else {
-            if (val && maxValue < val) {
-                maxValue = val;
-            }
-        }
-    });
-    return maxValue;
-}
-const ZOOM_MIN = 1.4;
+
 const ZOOM_MAX = 5;
-const ZOOM_MOUSE_MAX = 200000;
+const ZOOM_MOUSE = 0.01;
 
 function getImageZoom() {
-    let zoom = Math.min(100, CELL_IMAGE_ZOOM);
-    zoom = Math.max(0, zoom);
-    return lerp(ZOOM_MIN, ZOOM_MAX, zoom / 100);
+    if (CELL_IMAGE_ZOOM <= 0) {
+        const minScale = 1 / SUBDIVISION;
+        return lerp(minScale, 1, (100 + CELL_IMAGE_ZOOM) / 100);
+    } else {
+        return lerp(1, ZOOM_MAX, CELL_IMAGE_ZOOM / 100);
+    }
 }
 function getMouseImageZoom(imageSize: number) {
-    // imageZoom 0
-    if (SCALE_MOUSE_ZOOM > 0) {
-        return lerp(imageSize, imageSize * ZOOM_MOUSE_MAX, SCALE_MOUSE_ZOOM / 100);
+    if (SCALE_MOUSE_ZOOM <= 0) {
+        const minScale = 1 / SUBDIVISION;
+        return lerp(minScale, 1, (100 + SCALE_MOUSE_ZOOM) / 100);
     } else {
-        const removeScale = lerp(ZOOM_MIN, ZOOM_MAX, -SCALE_MOUSE_ZOOM / 100);
-        if (imageSize - removeScale < ZOOM_MIN) {
-            return ZOOM_MIN;
-        } else {
-            return removeScale;
-        }
+        return lerp(1, SCALE_MOUSE_ZOOM * SCALE_MOUSE_ZOOM * ZOOM_MOUSE, SCALE_MOUSE_ZOOM / 100);
     }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
-export const SUBDIVISION = 13;
+export const SUBDIVISION = 7;
 
-export const CELL_IMAGE_ZOOM = 1; //0 - 100
-export const SCALE_MOUSE_ZOOM = 100; //-100 - 100
+export const CELL_IMAGE_ZOOM = 0; //-100 - 100
+export const SCALE_MOUSE_ZOOM = 23; //-100 - 100
 export const ZOOM_RANGE_CELLS = 3;
 
-export const STIR_FREQUENCY = 100; //0-100
-export const STIR_FREQUENCY_BASE_TIME = 2; //seconds
-export const STIR_STRENGTH = 100; //0-100
+export const STIR_FREQUENCY = 25; //0-100
+export const STIR_FREQUENCY_BASE_TIME = 4; //seconds
+export const STIR_STRENGTH = 33; //0-100
 
-export const DISORDER = 5; //0-100
+export const DISORDER = 12; //0-100
 
-export const ZOOM_SPEED = 5; //0-100;
-export const FLIP_CELLS = true;
+export const ZOOM_SPEED = 100; //0-100;
+export const FLIP_CELLS = false;
 
 export enum DisplayMode {
     GRID = 'grid',
